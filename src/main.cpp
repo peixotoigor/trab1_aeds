58bc10b83671a7a9a2133bb002ae5536e1d5ff22
peixotoigor <igorpeixoto_@outlook.com.br>
2025-04-08 11:25:33 -0300

    Reorganização da estrutura, simplificação, somente função executar

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,6 @@
-#include "matriz_reader.hpp"
-#include <iostream>
+#include "Simulador.hpp"
 
 int main() {
-    // Criar o objeto matrixReader
-    matrixReader leitor;
-
-    // Carregar o arquivo de entrada
-    if (leitor.carregarArquivo("input.dat")) {
-        std::cout << "Arquivo carregado com sucesso!" << std::endl;
-
-        // Exibir as dimensões da matriz
-        std::cout << "Dimensões da matriz: " 
-                  << leitor.getComprimentoX() << " x " 
-                  << leitor.getComprimentoY() << std::endl;
-
-        // Exibir a posição inicial do incêndio
-        auto posicaoInicial = leitor.getPosicaoInicial();
-        std::cout << "Posição inicial do incêndio: (" 
-                  << posicaoInicial.first << ", " 
-                  << posicaoInicial.second << ")" << std::endl;
-
-        // Exibir a matriz
-        const auto& matriz = leitor.getMatriz();
-        std::cout << "Matriz da floresta:" << std::endl;
-        for (const auto& linha : matriz) {
-            for (char celula : linha) {
-                std::cout << celula << " ";
-            }
-            std::cout << std::endl;
-        }
-    } else {
-        std::cerr << "Erro ao carregar o arquivo!" << std::endl;
-    }
-
+    executarSimulacao();
     return 0;
 }
