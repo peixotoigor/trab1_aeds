cfafad0a231d88eaaf81dd3690a1c52228db0f80
Igor Peixoto <igorpeixoto_@outlook.com.brm>
2025-04-16 22:09:21 -0300

    Inclusão variável global, DIRECOES_ORTOGONAIS

--- src/config.hpp
+++ src/config.hpp
@@ -1,17 +1,44 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-const int MAX_ITERACOES =  10;
+#include <vector>
+#include <utility>
+
+// Número máximo de iterações
+const int MAX_ITERACOES = 10;
+
+// Número máximo de permanência
+const int MAX_PERMANENCIA = 3;
+
+// Ativar ou desativar o ventgo
 const bool VENTO_ATIVO = true;
 
-enum direcoesVento {
-    SEM_VENTO = 0,
-    ESQUERDA = 1,
-    DIREITA = 2,
-    ACIMA = 4,
-    ABAIXO = 8
+// Enum para identificar as direções do vento
+enum DIRECOES {
+    SEM_VENTO,
+    ESQUERDA,
+    DIREITA,
+    ACIMA,
+    ABAIXO
 };
 
-const int VENTO_DIRECAO = SEM_VENTO; // ESQUERDA | DIREITA;
+// Variável para definir as direções do vento (pode conter várias direções)
+const std::vector<DIRECOES> VENTO_DIRECOES = VENTO_ATIVO 
+    ? std::vector<DIRECOES>{ABAIXO, DIREITA} // Direções configuradas quando o vento está ativo
+    : std::vector<DIRECOES>{ESQUERDA, DIREITA, ACIMA, ABAIXO}; // Todas as direções quando SEM_VENTO
 
-#endif //CONFIG_HPP
+// Matriz de direções para cada tipo de vento
+const std::vector<std::pair<int, int>> DIRECOES_VENTO = {
+    {-1, 0}, // Cima
+    {1, 0},  // Baixo
+    {0, -1}, // Esquerda
+    {0, 1}   // Direita
+};
+
+const std::vector<std::pair<int, int>> DIRECOES_ORTOGONAIS = {
+    {-1, 0}, // Cima
+    {1, 0},  // Baixo
+    {0, -1}, // Esquerda
+    {0, 1}   // Direita
+};
+#endif // CONFIG_HPP
